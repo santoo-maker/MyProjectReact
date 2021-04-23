77b43c45c75c51a1c5edce089be47ce34a4c4c87
santooshrestha <shrestha.santoo@gmail.com>
2021-04-23 14:29:30 +0545

    made one login component for organizer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Route path = '/' exact component = {OrganizerLogin} />
+    
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
